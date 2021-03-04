7177151a564aa736fd7d6280053970139b9f0903
Martin Dev <u2001uk@yahoo.co.uk>
2021-03-04 18:12:46 +0000

    feature/added main settings icon
    eature/added main settings icon

--- js/index.js
+++ js/index.js
@@ -1,164 +1,170 @@
 class Timer {
+  constructor() {
+    this.type = "pomodoro";
+    this.timeLeft = 25;
+    this.clock = document.getElementById("time");
+    this.action = document.getElementById("action");
+    this.circle1 = document.getElementById("circle");
+    this.buttonshort = document.getElementById("buttonshort");
+    this.buttonlong = document.getElementById("buttonlong");
+    this.buttonpomo = document.getElementById("buttonpomo");
+    this.time = document.getElementById("time");
+    // this.audio1=document.getElementById("sound1")
+    this.time.innerHTML = this.timeLeft + ":00";
+    this.selectors = document.querySelectorAll("li");
+    this.timer = 25;
+    this.paused = false;
+    this.interval = 1000;
+    this.timetoContinue = 19;
+    this.timesetter = 25;
+    this.myInterval = null;
+    this.audio1 = document.getElementById("sound1");
+    // this.mainSettings = document.getElementById("settings1);
+    // console.log(this.mainSettings);
+  }
+  reset() {}
 
-    constructor() {
-        this.type = "pomodoro";
-        this.timeLeft = 25;
-        this.clock = document.getElementById("time");
-        this.action = document.getElementById("action");
-        this.circle1 = document.getElementById("circle");
-        this.buttonshort = document.getElementById("buttonshort");
-        this.buttonlong = document.getElementById("buttonlong");
-        this.buttonpomo = document.getElementById("buttonpomo")
-        this.time = document.getElementById("time")
-        // this.audio1=document.getElementById("sound1")
-        this.time.innerHTML = this.timeLeft + ":00";
-        this.selectors = document.querySelectorAll("li");
-        this.timer = 25;
-        this.paused = false;
-        this.interval = 1000;
-        this.timetoContinue = 19;
-        this.timesetter = 25;
-        this.myInterval = null;
-        this.audio1 = document.getElementById("sound1")
+  //   settingsMaster() {
+  //     this.mainsettings.addEventListener("click", (ev) => {
+  //       alert("window opened");
+  //             // window.open(url, windowName, [windowFeatures]);
+  //             // let options = 'titlebar=no,statusbar=no,height=300,width=600';
+  //             // let ref = wondow.open('new.html', 'Fred', options);
+  // });
+  //  }
+  // });
+  //   }
 
-    }
-    reset() {
-    }
+  settings() {
+    this.selectors.forEach((item) => {
+      // console.log(item.innerHTML)
+      // console.log(item.classList)
+      console.log("pressed");
+      if (item.classList == "active") {
+        let selectedwords = item.innerHTML;
+        let settime = 25;
+        // console.log(selectedwords)
 
-    settings() {
-        this.selectors.forEach((item, index) => {
-            // console.log(item.innerHTML)
-            // console.log(item.classList)
+        switch (selectedwords) {
+          case "pomodoro":
+            settime = 25;
+            break;
+          case "short break":
+            settime = 5;
+            break;
+          case "long break":
+            settime = 10;
+            break;
+        }
+        countdownTimer.start(settime);
+      }
+      // console.log(settime)
+    });
+    // console.log(this.settime)
+  }
 
-            if (item.classList == "active") {
-                let selectedwords = item.innerHTML
-                let settime = 25
-                // console.log(selectedwords)
+  resume() {
+    console.log("RESUME", this.timeLeft);
+    this.paused = false;
+    this.start(this.timeLeft / 60);
+  }
 
-                switch (selectedwords) {
-                    case "pomodoro":
-                        settime = 25;
-                        break;
-                    case "short break":
-                        settime = 5;
-                        break;
-                    case "long break":
-                        settime = 10;
-                        break;
-                }
-                countdownTimer.start(settime)
+  start(timeLeft) {
+    clearInterval(this.myInterval); // reset
+    console.log("TIMELEFTatstart", timeLeft);
+    this.action.innerHTML = "PAUSE";
+    this.timeLeft = timeLeft * 60;
+    this.startingtime = this.timeLeft;
+    let scope = this;
+    this.myInterval = setInterval(function () {
+      let seconds = Math.floor(scope.timeLeft % 60);
+      let minutes = Math.floor(scope.timeLeft / 60);
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+      if (scope.timeLeft == 0) {
+        console.log("HERE", scope.timeLeft);
+        clearInterval(scope.myInterval);
+        scope.clock.innerText = "00:00";
+        scope.audio1.play();
+        return;
+      }
+      console.log(scope.paused);
+      if (!scope.paused) {
+        console.log("NOT PAUSED");
+        // else { return };
+        scope.clock.innerText = `${minutes}:${seconds}`;
+        scope.circle1.style.strokeDashoffset =
+          scope.timeLeft / (scope.startingtime / 1024);
+        clearInterval(this.myInterval);
+      }
 
-            }
-            // console.log(settime)
-        })
-        // console.log(this.settime)
-    }
+      while (scope.paused) {
+        console.log("IS PAUSED");
+        scope.clock.innerText = `${minutes}:${seconds}`;
+        scope.circle1.style.strokeDashoffset =
+          scope.timeLeft / (scope.startingtime / 1024);
+        // let timo = scope.timeLeft;
+        scope.action.innerHTML = "RESUME";
 
-    resume() {
-        console.log("RESUME", this.timeLeft)
-        this.paused = false;
-        this.start(this.timeLeft / 60)
-    }
+        clearInterval(scope.myInterval);
+        // action(scope.action.innerHTML, timo);
+        return;
+      }
 
-    start(timeLeft) {
-        clearInterval(this.myInterval); // reset
-        console.log("TIMELEFTatstart", timeLeft)
-        this.action.innerHTML = "PAUSE"
-        this.timeLeft = timeLeft * 60 ;
-        this.startingtime = this.timeLeft;
-        let scope = this;
-        this.myInterval = setInterval(function () {
-            let seconds = Math.floor(scope.timeLeft % 60);
-            let minutes = Math.floor(scope.timeLeft / 60);
-            if (seconds < 10) { seconds = "0" + seconds }
-            if (minutes < 10) { minutes = "0" + minutes }
-            if (scope.timeLeft == 0) {
-                console.log("HERE",scope.timeLeft)
-                clearInterval(scope.myInterval);
-                scope.clock.innerText = "00:00";
-                scope.audio1.play();
-                return
-            }
-            console.log(scope.paused);
-            if (!scope.paused) {
-                console.log("NOT PAUSED")
-                // else { return };
-                scope.clock.innerText = `${minutes}:${seconds}`;
-                scope.circle1.style.strokeDashoffset = scope.timeLeft / (scope.startingtime / 1024);
-                clearInterval(this.myInterval);
-            }
+      console.log("outside-pause");
+      scope.timeLeft = scope.timeLeft - 1;
+      // console.log("IN START TIME LEFT", scope.timeLeft)
+    }, 1000);
+  }
 
-            while (scope.paused) {
-                console.log("IS PAUSED")
-                scope.clock.innerText = `${minutes}:${seconds}`;
-                scope.circle1.style.strokeDashoffset = scope.timeLeft / (scope.startingtime / 1024);
-                // let timo = scope.timeLeft;
-                scope.action.innerHTML = "RESUME";
-
-                clearInterval(scope.myInterval);
-                // action(scope.action.innerHTML, timo);
-                return
-            }
-
-            console.log("outside-pause")
-            scope.timeLeft = scope.timeLeft - 1
-            // console.log("IN START TIME LEFT", scope.timeLeft)
-        }, 1);
-
-    }
-
-    pause() {
-        console.log(this.timeLeft)
-        let scope1=this
-        this.paused = true;
-
-    }
-
-    
-    }
-    
-    
-
+  pause() {
+    console.log(this.timeLeft);
+    let scope1 = this;
+    this.paused = true;
+  }
+}
 
 const countdownTimer = new Timer();
 // countdownTimer.reset();
 function action(str) {
-    let apple=this.timeLeft
-    // alert("fired")
-    switch (str.toLowerCase()) {
-        case "start":
-            // alert("great")
-            countdownTimer.start(25)
-            break;
-        case "pause":
-            countdownTimer.pause()
-            break;
-        case "resume":
-            // console.log("THISTIMELEFT",this.timeLeft)
-            countdownTimer.resume()
-                break;
-        default:
-            countdownTimer.stop();
-            break;
-    }
+  let apple = this.timeLeft;
+  // alert("fired")
+  switch (str.toLowerCase()) {
+    case "start":
+      // alert("great")
+      countdownTimer.start(25);
+      break;
+    case "pause":
+      countdownTimer.pause();
+      break;
+    case "resume":
+      // console.log("THISTIMELEFT",this.timeLeft)
+      countdownTimer.resume();
+      break;
+    default:
+      countdownTimer.stop();
+      break;
+  }
 }
 
+// countdownTimer.settingsMaster();
 // this.selector()
 // countdownTimer.start()
-
 
 //NAV LINKS//
 
 const navbuttons = document.querySelectorAll("li");
 let navBg = document.getElementById("bgindicator");
 navbuttons.forEach((item, index) => {
-    navbuttons.item(index).addEventListener("click", (ev) => {
-        let movement = index * 118 + 25
-        navBg.style.marginLeft = `${movement}px`;
-        navbuttons.forEach(link => link.classList.remove("active"))
-        ev.target.classList.add("active")
-        countdownTimer.settings()
-
-    })
-}
-);
+  navbuttons.item(index).addEventListener("click", (ev) => {
+    let movement = index * 118 + 25;
+    navBg.style.marginLeft = `${movement}px`;
+    navbuttons.forEach((link) => link.classList.remove("active"));
+    ev.target.classList.add("active");
+    countdownTimer.settings();
+  });
+});
